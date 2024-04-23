e65ff52c96aa10c81ddec89a097d18486099ed35
angiellanos <aellanosc@unal.edu.co>
2024-04-22 22:15:16 -0500

    Se agregan todos los htmls creados hasta el momento

--- IBNR_calculo_completo.R
+++ IBNR_calculo_completo.R
@@ -2,18 +2,68 @@ if (!require('ChainLadder')) install.packages('ChainLadder')
 library(ChainLadder)
 library(readxl)
 
-##
-triangulo_reportados <- read_excel("C:/Users/joser/Downloads/prueba factor desarrollo 1.xlsx", sheet = "REPORTADOS")
-triangulo_pagados <- read_excel("C:/Users/joser/Downloads/prueba factor desarrollo 1.xlsx", sheet = "PAGADOS")
+if (!require('kableExtra')) install.packages('kableExtra')
+library(knitr)
+library(kableExtra)
 
-##
-triangulo_reportados <- as.data.frame(triangulo_reportados)
-rownames(triangulo_reportados) <- triangulo_reportados[,1]
-Reportados=triangulo_reportados[,-1]
+# Configurar R para evitar la notación científica
+options(scipen = 999)
 
-triangulo_pagados <- as.data.frame(triangulo_pagados)
-rownames(triangulo_pagados) <- triangulo_pagados[,1]
-Pagados=triangulo_pagados[,-1]
+
+# Definición de parámetros iniciales
+lambda <- 3  # Tasa promedio de reclamaciones por año de ocurrencia
+mu <- 10000  # Media de la distribución log-normal para la severidad
+sigma <- 3000  # Desviación estándar de la distribución log-normal para la severidad
+
+# Número de años de ocurrencia y desarrollo
+num_years_occurrence <- 10
+num_years_development <- 10
+years <- 2010:2019  # Años de ocurrencia
+months <- seq(12, 12 * num_years_development, by = 12)  # Meses de desarrollo
+
+# Creando matrices para almacenar los datos de los triángulos
+Reportados <- matrix(0, nrow = num_years_occurrence, ncol = num_years_development, dimnames = list(years, months))
+Pagados <- matrix(0, nrow = num_years_occurrence, ncol = num_years_development, dimnames = list(years, months))
+
+# Llenando el triángulo de reclamaciones con acumulados
+for (i in 1:num_years_occurrence) {
+  for (j in 1:(num_years_development - i + 1)) {
+    repeat {
+      num_claims <- rpois(1, lambda)
+      if (num_claims > 0) break
+    }
+    claim_sizes <- sum(rlnorm(num_claims, meanlog = log(mu), sdlog = log(sigma)))
+    Reportados[i, j] <- claim_sizes + if (j > 1) Reportados[i, j-1] else 0
+  }
+}
+
+# Definición de parámetros de pago
+pay_out_ratio_mean <- 0.8  # Media de la proporción de pago
+pay_out_ratio_sd <- 0.1    # Desviación estándar de la proporción de pago
+
+# Llenando el triángulo de pagos con acumulados asegurando que sean menores que los reportados
+for (i in 1:num_years_occurrence) {
+  for (j in 1:(num_years_development - i + 1)) {
+    payout_ratio <- min(rnorm(1, mean = pay_out_ratio_mean, sd = pay_out_ratio_sd), 0.99)
+    potential_payment <- payout_ratio * Reportados[i, j]
+    Pagados[i, j] <- potential_payment + if (j > 1) Pagados[i, j-1] else 0
+    # Asegurarse de que los pagados no superen a los reportados
+    Pagados[i, j] <- min(Pagados[i, j], Reportados[i, j])
+  }
+}
+
+# Imprimir los triángulos
+kable(Reportados, caption = "Triángulo de Reclamaciones Reportadas (Acumulados)") %>%
+  kable_styling(bootstrap_options = c("striped", "hover"))
+kable(Pagados, caption = "Triángulo de Pagos (Acumulados)") %>%
+  kable_styling(bootstrap_options = c("striped", "hover"))
+
+
+Pagados[Pagados == 0] <- NA
+Reportados[Reportados == 0] <- NA
+
+
+
 
 ##
 dev_factors_5 = function(t) {
@@ -70,7 +120,7 @@ for (i in 1:nrow(Pagados)) {
 
 ##
 ultimate_proj = data.frame(acc_year =row.names(Reportados),
-                           age_acc_year= rev(names(Reportados)),
+                           age_acc_year= rev(colnames(Reportados)),
                            Claims_REP=Claims_REP,
                            Claims_PAG=Claims_PAG,
                            CDF_REP=round(CDF_REP,3),
@@ -95,6 +145,12 @@ dev_unpaid_claims = data.frame(acc_year =row.names(Reportados),
 print(ultimate_proj)
 print(dev_unpaid_claims)
 ####################
+
+
+# Al imprimir o crear tablas con kable, formatea los números para mejorar la legibilidad
+# Reportados <- apply(Reportados, 1, function(x) format(x, big.mark = ",", decimal.mark = ".", scientific = FALSE))
+# Pagados <- apply(Pagados, 1, function(x) format(x, big.mark = ",", decimal.mark = ".", scientific = FALSE))
+
 
 
 
